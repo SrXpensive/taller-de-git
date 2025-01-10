df17d60b77d8679cf6b004954d363e6fdb8759b1
Edu <eduardo.caro.lorente@gmail.com>
2025-01-10 16:55:37 +0100

    Programa interactivo

--- lib/hola.php
+++ lib/hola.php
@@ -1,6 +1,6 @@
 <?php
 // Autor: Edu Caro <edu@edu.es>
-// El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
 ?>
