acf29f823b619ce1ff8cfb63c978afa23af02c6e
Edu <eduardo.caro.lorente@gmail.com>
2025-01-10 16:14:36 +0100

    Añadido el autor del programa y su email

--- hola.php
+++ hola.php
@@ -1,5 +1,6 @@
 <?php
+// Autor: Edu Caro <edu@edu.es>
 // El nombre por defecto es Mundo
 $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";j
+@print "Hola, {$nombre}\n";
 ?>
